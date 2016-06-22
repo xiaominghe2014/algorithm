eb64ab5c5ce8f02a7f4de4174b8bda5d25f7ef74
ximena <ximena@Ximena.local>
2016-06-22 11:38:10 +0800

    first init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by ximena on 16/6/22.
+//  Copyright © 2016年 ximena. All rights reserved.
+//
+
+#include <iostream>
+
+#include "src/base/tree.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    tree::bstree::test().run();
+    return 0;
+}
